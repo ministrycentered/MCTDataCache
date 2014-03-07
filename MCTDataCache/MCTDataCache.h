c92b8590d22c920f8aa9bbdefb2e0e49bf6941e3
Skylar Schipper <ss@schipp.co>
2014-03-07 09:11:43 -0800

    Initial Commit

---
+++ MCTDataCache/MCTDataCache.h
@@ -0,0 +1,13 @@
+//
+//  MCTDataCache.h
+//  MCTDataCache
+//
+//  Created by Skylar Schipper on 3/7/14.
+//  Copyright (c) 2014 Ministry Centered Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCTDataCache : NSObject
+
+@end
