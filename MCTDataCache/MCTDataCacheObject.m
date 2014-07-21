a139a38697e2455ffbe9301a844e41a2af98a270
Skylar Schipper <ss@schipp.co>
2014-07-21 15:44:23 -0700

    Update MCTDataCacheObject.m

--- MCTDataCache/MCTDataCacheObject.m
+++ MCTDataCache/MCTDataCacheObject.m
@@ -14,6 +14,7 @@
 
 @implementation MCTDataCacheObject
 @synthesize info = _info;
+@synthesize hash = _hash;
 
 - (instancetype)initWithHash:(NSString *)hash rootPath:(NSString *)rootPath filePath:(NSString *)filePath fileInfoPath:(NSString *)fileInfoPath {
     self = [super init];
